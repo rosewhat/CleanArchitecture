9be3fadde371bcfa1c8c1478ece1dbfd67f008e1
rosewhat <whatcodingrose@gmail.com>
2022-10-17 21:33:28 +0300

    unit test

---
+++ domain/src/test/java/com/rosewhat/cleanarchitecture/domain/usecases/GetUserNameUseCaseTest.kt
@@ -0,0 +1,28 @@
+package com.rosewhat.cleanarchitecture.domain.usecases
+
+import com.rosewhat.cleanarchitecture.domain.model.UserName
+import com.rosewhat.cleanarchitecture.domain.repository.UserRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+
+class GetUserNameUseCaseTest {
+
+    val userRepository = mock<UserRepository>()
+
+    @Test
+    fun `should return the same data as it repository`() {
+
+        val testUserName = UserName(firstName = "test firstName", lastName = "test last name")
+        Mockito.`when`(userRepository.getName()).thenReturn(testUserName)
+
+        val actual = GetUserNameUseCase(userRepository = userRepository).getName()
+
+        val expected = UserName(firstName = "test firstName", lastName = "test last name")
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+}
